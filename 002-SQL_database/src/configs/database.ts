54a2c6f4a7b7b6efa01a1c99c51e32aa2931373e
Tu Huynh <huynhlethanhtu@gmail.com>
2024-08-01 03:22:56 +0700

    NoSQL database feature

--- 002-SQL_database/src/configs/database.ts
+++ 002-SQL_database/src/configs/database.ts
@@ -3,10 +3,11 @@ const mysqlDriver = require("mysql2");
 require("dotenv").config();
 
 const dbPool = mysqlDriver.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
 });
 
 export default dbPool.promise();
